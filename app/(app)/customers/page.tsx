d4f5b477d909828261e835b60c68f7559cfae34a
Simon Gjøen <s_gjoen@hotmail.com>
2024-02-10 13:16:26 +0100

    feature: update customer page
    The customer page can now:
    - display mockup customers by using cards
    - display more details for the customer in a modal by clicking on "details"

--- app/(app)/customers/page.tsx
+++ app/(app)/customers/page.tsx
@@ -1,9 +1,35 @@
+'use client';
+
 import { Title } from '@mantine/core';
+import { useState } from 'react';
+import { CustomerCard } from './CustomerCard/CustomerCard';
+import { CustomerModal } from './CustomerModal/CustomerModal';
+import { mockCustomers } from './data';
+import { Customer } from './types';
 
 export default function CustomersPage() {
+  const [selectedCustomer, setSelectedCustomer] = useState<Customer | null>(null);
+  const [modalOpened, setModalOpened] = useState(false);
+
+  const openModal = (customer: Customer) => {
+    setSelectedCustomer(customer);
+    setModalOpened(true);
+  };
+
   return (
     <>
-      <Title>Kunder</Title>
+      <Title style={{ paddingBottom: '20px' }}>Kunder</Title>
+      <div style={{ maxWidth: 800, margin: 'auto' }}>
+        {mockCustomers.map((customer) => (
+          <CustomerCard key={customer.id} customer={customer} onOpenModal={openModal} />
+        ))}
+      </div>
+
+      <CustomerModal
+        customer={selectedCustomer}
+        opened={modalOpened}
+        onClose={() => setModalOpened(false)}
+      />
     </>
   );
 }
