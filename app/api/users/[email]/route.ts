f80a81e4322122b5a0b39ed272d3eefccec526d0
Mark Kour <mark.kour@icloud.com>
2024-02-13 21:56:57 +0100

    feat: add modify user handler

--- app/api/users/[email]/route.ts
+++ app/api/users/[email]/route.ts
@@ -29,3 +29,47 @@ export async function GET(req: NextRequest, { params }: { params: { email: strin
     });
   }
 }
+
+export async function PUT(req: NextRequest, { params }: { params: { email: string } }) {
+  const { email } = params;
+
+  if (!email) {
+    return new NextResponse(JSON.stringify({ error: 'User email is not defined' }), {
+      status: 400,
+    });
+  }
+
+  try {
+    const userData = await req.json();
+    if (!userData) {
+      return new NextResponse(JSON.stringify({ error: 'User data is not defined' }), {
+        status: 400,
+      });
+    }
+
+    const tenant = await prisma.tenant.create({
+      data: {
+        id: userData.tenantId,
+        name: userData.tenantName,
+      },
+    });
+
+    const updatedUser = await prisma.user.update({
+      where: { email },
+      data: {
+        firstName: userData.firstName,
+        lastName: userData.lastName,
+        tenantId: tenant.id,
+      },
+    });
+
+    return new NextResponse(JSON.stringify(updatedUser), {
+      status: 200,
+      statusText: 'User created successfully',
+    });
+  } catch (error) {
+    return new NextResponse(JSON.stringify({ error: 'Failed to create a user' }), {
+      status: 500,
+    });
+  }
+}
