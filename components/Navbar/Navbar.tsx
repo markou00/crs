253ff49af739eb9e0e3f77b1eb8e1cc8a9fa41a2
Mark Kour <mark.kour@icloud.com>
2024-02-15 00:32:12 +0100

    fix: dynamic navbar links

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -11,29 +11,54 @@ import {
   IconBox,
   IconLayoutDashboard,
 } from '@tabler/icons-react';
+import { useEffect, useState } from 'react';
+import { createClientComponentClient, User } from '@supabase/auth-helpers-nextjs';
 import { useMediaQuery } from '@mantine/hooks';
 import { usePathname } from 'next/navigation';
 import { UserButton } from './UserButton/UserButton';
 import classes from './Navbar.module.css';
 import useNavbarStore from '@/lib/states/useNavbarDisclosure';
 
-const data = [
-  { link: '/dashboard', label: 'Dashboard', icon: IconLayoutDashboard },
-  { link: '/customers', label: 'Kunder', icon: IconUsers },
-  { link: '/employees', label: 'Ansatte', icon: IconBriefcase },
-  { link: '/agreements', label: 'Avtaler', icon: IconClipboard },
-  { link: '/jobs', label: 'Oppdrag', icon: IconCheckbox },
-  { link: '/trucks', label: 'Biler', icon: IconTruck },
-  { link: '/dispatch', label: 'Planlegging', icon: IconColumns3 },
-  { link: '/containers', label: 'Beholdere', icon: IconBox },
-  { link: '/settings', label: 'Innstillinger', icon: IconSettings },
-];
-
 export function Navbar() {
   const pathname = usePathname();
   const theme = useMantineTheme();
   const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
   const toggle = useNavbarStore((state) => state.toggle);
+  const [user, setUser] = useState<User>();
+  const supabase = createClientComponentClient();
+
+  useEffect(() => {
+    const getUser = async () => {
+      const { data } = await supabase.auth.getUser();
+      data.user && setUser(data.user);
+    };
+
+    getUser();
+  }, []);
+
+  const data = [
+    {
+      link: `/${user?.user_metadata?.tenantId}/dashboard`,
+      label: 'Dashboard',
+      icon: IconLayoutDashboard,
+    },
+    { link: `/${user?.user_metadata?.tenantId}/customers`, label: 'Kunder', icon: IconUsers },
+    { link: `/${user?.user_metadata?.tenantId}/employees`, label: 'Ansatte', icon: IconBriefcase },
+    { link: `/${user?.user_metadata?.tenantId}/agreements`, label: 'Avtaler', icon: IconClipboard },
+    { link: `/${user?.user_metadata?.tenantId}/jobs`, label: 'Oppdrag', icon: IconCheckbox },
+    { link: `/${user?.user_metadata?.tenantId}/trucks`, label: 'Biler', icon: IconTruck },
+    {
+      link: `/${user?.user_metadata?.tenantId}/dispatch`,
+      label: 'Planlegging',
+      icon: IconColumns3,
+    },
+    { link: `/${user?.user_metadata?.tenantId}/containers`, label: 'Beholdere', icon: IconBox },
+    {
+      link: `/${user?.user_metadata?.tenantId}/settings`,
+      label: 'Innstillinger',
+      icon: IconSettings,
+    },
+  ];
 
   const links = data.map((item) => (
     <Link
