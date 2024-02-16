e9dd8af8dfa841d963e7be0989081ca33cac84ef
Simon Gjøen <s_gjoen@hotmail.com>
2024-02-16 17:09:51 +0100

    feat: edit customer
    Edit a customer by clicking "mer info", change the info, and "save".

--- app/(app)/customers/[customerId]/page.tsx
+++ app/(app)/customers/[customerId]/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import Link from 'next/link';
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation'; // Import useRouter for navigation
+import { useRouter } from 'next/navigation';
+import { Title, TextInput, Button, Group, Stack } from '@mantine/core';
 import { Customer } from './types';
 
 interface Params {
@@ -11,6 +11,7 @@ interface Params {
 
 export default function CustomerDetails({ params }: { params: Params }) {
   const [customer, setCustomer] = useState<Customer | null>(null);
+  const [editCustomer, setEditCustomer] = useState<Customer | null>(null);
   const [error, setError] = useState('');
   const router = useRouter();
 
@@ -37,7 +38,11 @@ export default function CustomerDetails({ params }: { params: Params }) {
   }, [params.customerId]);
 
   const deleteCustomer = async () => {
-    if (window.confirm('Are you sure you want to delete this customer?')) {
+    if (
+      window.confirm(
+        'Er du sikker på at du vil slette denne kunden? Denne handlingen kan ikke angres.'
+      )
+    ) {
       try {
         const response = await fetch(`/api/customers/${params.customerId}`, {
           method: 'DELETE',
@@ -56,25 +61,112 @@ export default function CustomerDetails({ params }: { params: Params }) {
     }
   };
 
+  useEffect(() => {
+    setEditCustomer(customer); // Initialize editable customer state with fetched data
+  }, [customer]);
+
+  const handleInputChange = (field: keyof Customer, value: string) => {
+    setEditCustomer((prevState) => {
+      // Check if prevState is null to maintain type safety
+      if (prevState === null) return null;
+
+      // Correctly updates prevState with the new field value
+      return { ...prevState, [field]: value };
+    });
+  };
+
+  const saveChanges = async (returnToCustomers = false) => {
+    if (!editCustomer) return;
+    try {
+      const response = await fetch(`/api/customers/${params.customerId}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(editCustomer),
+      });
+      if (!response.ok) throw new Error('Failed to update the customer');
+
+      if (returnToCustomers) {
+        router.push('/customers');
+      } else {
+        const updatedCustomer = await response.json();
+        setCustomer(updatedCustomer);
+        setEditCustomer(updatedCustomer);
+      }
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError('An unexpected error occurred while updating the customer');
+      }
+    }
+  };
+
   if (error) return <div>An error occurred: {error}</div>;
-  if (!customer) return <div>Loading...</div>;
+  if (!editCustomer) return <div>Loading...</div>;
 
   return (
     <div>
-      <h1>{customer.name}</h1>
-      <p>Kontaktnavn: {customer.contactName}</p>
-      <p>Epost: {customer.contactEmail}</p>
-      <p>Tlf: {customer.contactPhone || 'N/A'}</p>
-      <p>Adresse: {customer.address}</p>
-      <p>Sted: {customer.city}</p>
-      <p>Postnr: {customer.postalCode}</p>
-      <p>Land: {customer.country}</p>
-      <button type="button" onClick={deleteCustomer}>
-        Delete Customer
-      </button>
-      <p>
-        <Link href="/customers">Tilbake til kundesiden</Link>
-      </p>
+      <Title>Kundeinformasjon</Title>
+      <Group pt={30} pb={30}>
+        <Stack align="flex-start" justify="flex-start">
+          <TextInput
+            label="Navn"
+            value={editCustomer.name || ''}
+            onChange={(event) => handleInputChange('name', event.currentTarget.value)}
+          />
+          <TextInput
+            label="Kontaktnavn"
+            value={editCustomer.contactName || ''}
+            onChange={(event) => handleInputChange('contactName', event.currentTarget.value)}
+          />
+          <TextInput
+            label="Epost"
+            value={editCustomer.contactEmail || ''}
+            onChange={(event) => handleInputChange('contactEmail', event.currentTarget.value)}
+          />
+          <TextInput
+            label="Telefon"
+            value={editCustomer.contactPhone || ''}
+            onChange={(event) => handleInputChange('contactPhone', event.currentTarget.value)}
+          />
+        </Stack>
+        <Stack align="flex-start" justify="flex-start">
+          <TextInput
+            label="Adresse"
+            value={editCustomer.address || ''}
+            onChange={(event) => handleInputChange('address', event.currentTarget.value)}
+          />
+          <TextInput
+            label="Sted"
+            value={editCustomer.city || ''}
+            onChange={(event) => handleInputChange('city', event.currentTarget.value)}
+          />
+          <TextInput
+            label="Postnr"
+            value={editCustomer.postalCode || ''}
+            onChange={(event) => handleInputChange('postalCode', event.currentTarget.value)}
+          />
+          <TextInput
+            label="Land"
+            value={editCustomer.country || ''}
+            onChange={(event) => handleInputChange('country', event.currentTarget.value)}
+          />
+        </Stack>
+      </Group>
+      <Group>
+        <Stack>
+          <Button variant="default" onClick={() => router.push('/customers')}>
+            Avbryt
+          </Button>
+          <Button color="red" onClick={deleteCustomer}>
+            Slett kunde
+          </Button>
+        </Stack>
+        <Stack>
+          <Button onClick={() => saveChanges()}>Lagre</Button>
+          <Button onClick={() => saveChanges(true)}>Lagre og gå til kundeoversikt</Button>
+        </Stack>
+      </Group>
     </div>
   );
 }
