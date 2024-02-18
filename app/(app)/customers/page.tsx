777112ea45806a281088b66171017da8d2f5d3e1
Mark Kour <mark.kour@icloud.com>
2024-02-18 23:07:21 +0100

    feat: add a search bar to filter customers by name

--- app/(app)/customers/page.tsx
+++ app/(app)/customers/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { Title, Button, Group } from '@mantine/core';
-import { IconUserPlus } from '@tabler/icons-react';
+import { Title, Button, Group, TextInput, rem } from '@mantine/core';
+import { IconSearch, IconUserPlus } from '@tabler/icons-react';
 import { useEffect, useState } from 'react';
 import { CustomerCard } from './CustomerCard/CustomerCard';
 import { AddCustomerModal } from './AddCustomerModal/AddCustomerModal';
@@ -10,6 +10,7 @@ import { Customer } from './types';
 export default function CustomersPage() {
   const [customers, setCustomers] = useState<Customer[]>([]);
   const [addModalOpened, setAddModalOpened] = useState(false);
+  const [search, setSearch] = useState('');
 
   // Function to open the modal for creating a new customer
   const openCreateModal = () => {
@@ -44,16 +45,28 @@ export default function CustomersPage() {
     fetchCustomers();
   };
 
+  const filteredCustomers = customers.filter((customer) =>
+    customer.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <>
       <Group justify="space-between" style={{ marginBottom: '20px', marginRight: '50px' }}>
         <Title>Kunder</Title>
+        <TextInput
+          radius="xl"
+          size="md"
+          placeholder="Search questions"
+          rightSectionWidth={42}
+          leftSection={<IconSearch style={{ width: rem(18), height: rem(18) }} stroke={1.5} />}
+          onChange={(event) => setSearch(event.currentTarget.value)}
+        />
         <Button onClick={openCreateModal} leftSection={<IconUserPlus size={16} />}>
           Ny kunde
         </Button>
       </Group>
       <div style={{ maxWidth: 800, margin: 'auto' }}>
-        {customers.map((customer) => (
+        {filteredCustomers.map((customer) => (
           <CustomerCard key={customer.id} customer={customer} />
         ))}
       </div>
