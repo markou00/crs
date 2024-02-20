6bcef6b46214e80752d8d06969c420af7aae21f4
Mark Kour <mark.kour@icloud.com>
2024-02-20 20:56:41 +0100

    feat: add invite modal

--- app/(app)/[tenantId]/settings/page.tsx
+++ app/(app)/[tenantId]/settings/page.tsx
@@ -1,9 +1,35 @@
-import { Title } from '@mantine/core';
+'use client';
+
+import { Group, Title, Modal, Button, TextInput, Flex, Divider } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { IconAt, IconSend, IconUsersPlus } from '@tabler/icons-react';
 
 export default function SettignsPage() {
+  const [opened, { open, close }] = useDisclosure(false);
+
   return (
     <>
-      <Title>Innstillinger</Title>
+      <Group justify="space-between">
+        <Title>Innstillinger</Title>
+        <Button onClick={open} leftSection={<IconUsersPlus />}>
+          Inviter
+        </Button>
+      </Group>
+
+      <Divider mt="sm" mb="sm" />
+
+      <Modal opened={opened} onClose={close} title="Invitere brukere">
+        <Flex direction="column" gap="md">
+          <TextInput
+            data-autofocus
+            label="E-post addresse til den du vil invitere:"
+            placeholder="hello@example.com"
+            leftSection={<IconAt width="1.2rem" />}
+            mt="md"
+          />
+          <Button leftSection={<IconSend width="1.4rem" />}>Send</Button>
+        </Flex>
+      </Modal>
     </>
   );
 }
