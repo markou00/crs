ec216809ce088fb2cf6bfce751fd80c27628a277
Mark Kour <mark.kour@icloud.com>
2024-02-20 22:38:51 +0100

    fix: create triggers in the same seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,52 @@
 import { PrismaClient } from '@prisma/client';
 import { createClient } from '@supabase/supabase-js';
+import postgres from 'postgres';
+
+import 'dotenv/config';
+
+const dbUrl = process.env.DATABASE_URL;
+
+if (!dbUrl) {
+  throw new Error("Couldn't find db url");
+}
+
+const sql = postgres(dbUrl);
 
 const prisma = new PrismaClient();
 
 async function main() {
+  await sql`
+        create or replace function public.handle_new_user()
+        returns trigger as $$
+        begin
+            insert into public."User" (id, email)
+            values (new.id, new.email);
+            return new;
+        end;
+        $$ language plpgsql security definer;
+        `;
+  await sql`
+        create or replace trigger on_auth_user_created
+            after insert on auth.users
+            for each row execute procedure public.handle_new_user();
+      `;
+
+  await sql`
+        create or replace function public.handle_user_delete()
+        returns trigger as $$
+        begin
+          delete from auth.users where id = old.id;
+          return old;
+        end;
+        $$ language plpgsql security definer;
+      `;
+
+  await sql`
+        create or replace trigger on_user_deleted 
+          after delete on public."User"
+          for each row execute procedure public.handle_user_delete();
+      `;
+
   const tenant = await prisma.tenant.create({
     data: { id: 'CRS', name: 'Container Rental System' },
   });
@@ -99,6 +142,7 @@ async function main() {
     ],
   });
   console.log('Created customers:', customers.count);
+  process.exit();
 }
 
 main()
