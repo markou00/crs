ca304360809b43bdf2a49abb0fd92d7852575051
Simon Gjøen <s_gjoen@hotmail.com>
2024-02-22 16:49:49 +0100

    fix: datatable css not working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
-import '@mantine/core/styles.css';
-
 import { ColorSchemeScript } from '@mantine/core';
 import Provider from '@/lib/Providers';
 
+// import '@mantine/core/styles.css';
 import '@mantine/core/styles.layer.css';
 import 'mantine-datatable/styles.layer.css';
 import './layout.css';
