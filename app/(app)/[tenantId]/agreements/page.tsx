56390beac8d65c35e07266186fce530807919a46
Mark Kour <mark.kour@icloud.com>
2024-02-27 23:14:18 +0100

    feat: filter agreements by id

--- app/(app)/[tenantId]/agreements/page.tsx
+++ app/(app)/[tenantId]/agreements/page.tsx
@@ -1,8 +1,11 @@
 'use client';
 
-import { Box, Text, Title } from '@mantine/core';
+import { ActionIcon, Box, Text, TextInput, Title } from '@mantine/core';
 import { useQuery } from '@tanstack/react-query';
 import { DataTable } from 'mantine-datatable';
+import { useEffect, useState } from 'react';
+import { IconSearch, IconX } from '@tabler/icons-react';
+import { useDebouncedValue } from '@mantine/hooks';
 
 import { getAgreements } from '@/lib/server/actions/agreements-actions';
 
@@ -11,6 +14,28 @@ export default function AgreementsPage() {
     queryKey: ['agreements'],
     queryFn: () => getAgreements(),
   });
+
+  const initialRecords = getAgreementsQuery.data?.agreements;
+
+  const [records, setRecords] = useState(initialRecords);
+
+  const [idQuery, setIdQuery] = useState('');
+  const [debouncedIdQuery] = useDebouncedValue(idQuery, 200);
+
+  useEffect(() => {
+    setRecords(
+      initialRecords?.filter(({ id }) => {
+        if (
+          debouncedIdQuery !== '' &&
+          !`${id}`.toLowerCase().includes(debouncedIdQuery.trim().toLowerCase())
+        ) {
+          return false;
+        }
+
+        return true;
+      })
+    );
+  }, [debouncedIdQuery]);
 
   if (getAgreementsQuery.error) return <Text>ERROR....</Text>;
   if (getAgreementsQuery.isLoading) return <Text>LOADING...</Text>;
@@ -24,10 +49,31 @@ export default function AgreementsPage() {
         withColumnBorders
         striped
         highlightOnHover
-        records={getAgreementsQuery.data?.agreements}
+        records={records}
         columns={[
           {
             accessor: 'id',
+            filter: (
+              <TextInput
+                label="Id"
+                description="Show agreements whose id include the specified id"
+                placeholder="Search agreements..."
+                leftSection={<IconSearch size={16} />}
+                rightSection={
+                  <ActionIcon
+                    size="sm"
+                    variant="transparent"
+                    c="dimmed"
+                    onClick={() => setIdQuery('')}
+                  >
+                    <IconX size={14} />
+                  </ActionIcon>
+                }
+                value={idQuery}
+                onChange={(e) => setIdQuery(e.currentTarget.value)}
+              />
+            ),
+            filtering: idQuery !== '',
           },
           { accessor: 'type' },
           {
