1c6c8a7b9235c4616f12fed3b5e4b05f48667602
Mark Kour <mark.kour@icloud.com>
2024-03-03 10:56:37 +0100

    fix: return only the agreements on the tenant

--- lib/server/actions/agreements-actions.ts
+++ lib/server/actions/agreements-actions.ts
@@ -1,10 +1,19 @@
 'use server';
 
+import { createServerActionClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+
 import prisma from '@/lib/prisma';
 
 export async function getAgreements() {
   try {
+    const supabase = createServerActionClient({ cookies });
+
+    const authUser = await supabase.auth.getUser();
+    const tenantId = authUser.data.user?.user_metadata.tenantId;
+
     const agreements = await prisma.agreement.findMany({
+      where: { tenantId },
       include: {
         customer: true,
         container: true,
