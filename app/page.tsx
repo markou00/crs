af2d838e05bda48a0a150e6a210ea9031e194142
Simon Gjøen <s_gjoen@hotmail.com>
2024-04-09 16:00:12 +0200

    feat: add URL to the web page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,14 @@ export default function HomePage() {
   return (
     <>
       <div>
-        Gå til: <a href="http://localhost:3000/dashboard">http://localhost:3000/dashboard</a>
+        <p>
+          Gå til: <a href="http://localhost:3000/login">http://localhost:3000/login</a> for lokal
+          hosting.
+        </p>
+        <p>
+          Gå til: <a href="https://crs-ecru.vercel.app/login">https://crs-ecru.vercel.app/login</a>{' '}
+          for nettsiden.
+        </p>
       </div>
     </>
   );
